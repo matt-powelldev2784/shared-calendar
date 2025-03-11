7d2484844fb758a4a10077366edc94c342103c82
Matthew Powell <matt.powell2784@gmail.com>
2025-03-11 21:19:05 +0000

    feat: add hours and minute form elements to add entry component

--- src/components/addEntry/addEntry.tsx
+++ src/components/addEntry/addEntry.tsx
@@ -23,13 +23,18 @@ import Error from '../ui/error';
 import type { CustomError } from '@/ts/errorClass';
 import { useQuery } from '@tanstack/react-query';
 import { Input } from '../ui/input';
+import { Textarea } from '../ui/textarea';
 
 const formSchema = z.object({
   calendarId: z.string().nonempty(),
   title: z.string().nonempty(),
-  // description: z.string().optional(),
-  // startDate: z.date(),
-  // endDate: z.date(),
+  description: z.string().optional(),
+  startDateHour: z.number().min(0).max(23),
+  startDateTenMinIntervals: z.number().min(0).max(9),
+  startDateOneMinIntervals: z.number().min(0).max(9),
+  endDateHour: z.number().min(0).max(23),
+  endDateTenMinIntervals: z.number().min(0).max(9),
+  endDateOneMinIntervals: z.number().min(0).max(9),
   // ownerIds: z.array(z.string()).nonempty(),
   // subscribers: z.array(z.string()),
   // pendingRequests: z.array(z.string()),
@@ -50,9 +55,13 @@ const AddEntry = () => {
     defaultValues: {
       calendarId: '',
       title: '',
-      // description: '',
-      // startDate: new Date(),
-      // endDate: new Date(),
+      description: '',
+      startDateHour: 9,
+      startDateTenMinIntervals: 0,
+      startDateOneMinIntervals: 0,
+      endDateHour: 10,
+      endDateTenMinIntervals: 0,
+      endDateOneMinIntervals: 0,
       // ownerIds: [],
       // subscribers: [],
       // pendingRequests: [],
@@ -120,6 +129,184 @@ const AddEntry = () => {
             </FormItem>
           )}
         />
+
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem className="m-4 w-full max-w-[700px]">
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Textarea placeholder="Description" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <div className="flex w-full max-w-[700px] flex-row justify-between">
+          <div>
+            <FormLabel className="mb-2">Start Time</FormLabel>
+            <div className="flex flex-row">
+              <FormField
+                control={form.control}
+                name="startDateHour"
+                render={({ field }) => (
+                  <FormItem className="m-0 w-20 max-w-[700px]">
+                    <FormLabel className="sr-only">Start Hour</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Start Hour"
+                        className="text-center"
+                        type="number"
+                        {...field}
+                        onChange={(e) => field.onChange(e.target.valueAsNumber)}
+                        style={{
+                          WebkitAppearance: 'none',
+                          MozAppearance: 'textfield',
+                        }}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <p className="mx-2 translate-y-0.5 text-xl font-bold">:</p>
+
+              <FormField
+                control={form.control}
+                name="startDateTenMinIntervals"
+                render={({ field }) => (
+                  <FormItem className="m-0 w-20 max-w-[700px]">
+                    <FormLabel className="sr-only">
+                      Start Minute in 10 minute intervals
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Start Minute in 10 minute intervals"
+                        className="text-center"
+                        type="number"
+                        {...field}
+                        onChange={(e) => field.onChange(e.target.valueAsNumber)}
+                        style={{
+                          WebkitAppearance: 'none',
+                          MozAppearance: 'textfield',
+                        }}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="startDateOneMinIntervals"
+                render={({ field }) => (
+                  <FormItem className="m-0 w-20 max-w-[700px]">
+                    <FormLabel className="sr-only">
+                      Start Minute in 10 minute intervals
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Start Minute in 10 minute intervals"
+                        className="text-center"
+                        type="number"
+                        {...field}
+                        onChange={(e) => field.onChange(e.target.valueAsNumber)}
+                        style={{
+                          WebkitAppearance: 'none',
+                          MozAppearance: 'textfield',
+                        }}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
+
+          <div>
+            <FormLabel className="mb-2">End Time</FormLabel>
+            <div className="flex flex-row">
+              <FormField
+                control={form.control}
+                name="endDateHour"
+                render={({ field }) => (
+                  <FormItem className="m-0 w-20 max-w-[700px]">
+                    <FormLabel className="sr-only">Start Hour</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Start Hour"
+                        className="text-center"
+                        type="number"
+                        {...field}
+                        onChange={(e) => field.onChange(e.target.valueAsNumber)}
+                        style={{
+                          WebkitAppearance: 'none',
+                          MozAppearance: 'textfield',
+                        }}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <p className="mx-2 translate-y-0.5 text-xl font-bold">:</p>
+
+              <FormField
+                control={form.control}
+                name="endDateTenMinIntervals"
+                render={({ field }) => (
+                  <FormItem className="m-0 w-20 max-w-[700px]">
+                    <FormLabel className="sr-only">
+                      Start Minute in 10 minute intervals
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Start Minute in 10 minute intervals"
+                        className="text-center"
+                        type="number"
+                        {...field}
+                        onChange={(e) => field.onChange(e.target.valueAsNumber)}
+                        style={{
+                          WebkitAppearance: 'none',
+                          MozAppearance: 'textfield',
+                        }}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="endDateOneMinIntervals"
+                render={({ field }) => (
+                  <FormItem className="m-0 w-20 max-w-[700px]">
+                    <FormLabel className="sr-only">
+                      Start Minute in 10 minute intervals
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Start Minute in 10 minute intervals"
+                        className="text-center"
+                        type="number"
+                        {...field}
+                        onChange={(e) => field.onChange(e.target.valueAsNumber)}
+                        style={{
+                          WebkitAppearance: 'none',
+                          MozAppearance: 'textfield',
+                        }}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
+        </div>
 
         <Button type="submit">Submit</Button>
       </form>
