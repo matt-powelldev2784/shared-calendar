0b268be04dc386505f8822916d94ecc47ebf2a3d
Matthew Powell <matt.powell2784@gmail.com>
2025-03-16 19:30:22 +0000

    refactor: change calendar page component to be standard function

--- src/routes/add-entry.tsx
+++ src/routes/add-entry.tsx
@@ -2,10 +2,10 @@ import AddEntry from '@/components/addEntry/addEntry';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/add-entry')({
-  component: AddEntry,
+  component: AddEntryPage,
 });
 
-const AddEntryPage = () => {
+function AddEntryPage() {
   return (
     <section className="flex h-full w-full flex-col items-center justify-center">
       <AddEntry />
