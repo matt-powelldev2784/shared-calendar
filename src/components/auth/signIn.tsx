d502305babffdd55b9522adc74a6b3ead943ab21
Matthew Powell <matt.powell2784@gmail.com>
2025-03-19 21:46:20 +0000

    refacor: move sign in with google fucntion to db folder

--- src/components/auth/signIn.tsx
+++ src/components/auth/signIn.tsx
@@ -1,6 +1,3 @@
-import { signInWithPopup } from 'firebase/auth';
-import { GoogleAuthProvider } from 'firebase/auth';
-import { auth } from '@/db/firebaseConfig';
 import {
   Card,
   CardHeader,
@@ -10,6 +7,7 @@ import {
 } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import googleGIcon from '@/assets/icons/google_g_logo.svg';
+import { SignInWithGoogle } from '@/db/signInWithGoogle';
 
 export const SignIn = () => {
   return (
@@ -37,30 +35,4 @@ export const SignIn = () => {
       </CardContent>
     </Card>
   );
-};
-
-const SignInWithGoogle = async () => {
-  try {
-    const provider = new GoogleAuthProvider();
-    const result = await signInWithPopup(auth, provider);
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    if (!credential) {
-      console.error('Error in user Credential');
-      return;
-    }
-    const token = credential.accessToken;
-    const user = result.user;
-    console.log(user, token);
-  } catch (error: any) {
-    console.error('Error during sign-in:', error);
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    const email = error.customData?.email;
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    console.log('errorCode', errorCode);
-    console.log('errorMessage', errorMessage);
-    console.log('email', email);
-    console.log('credential', credential);
-    throw error;
-  }
 };
