f861bf16bd7ede925ee71e4f86b25f3e675af230
Matthew Powell <matt.powell2784@gmail.com>
2025-03-25 20:48:25 +0000

    feat: add error page

---
+++ src/routes/error.tsx
@@ -0,0 +1,36 @@
+import Error from '@/components/ui/error';
+import type { CustomError } from '@/ts/errorClass';
+import { createFileRoute, useLoaderData } from '@tanstack/react-router';
+import { z } from 'zod';
+
+const errorSearchSchema = z.object({
+  status: z.union([z.string(), z.number()]),
+  message: z.string(),
+  name: z.string().optional(),
+});
+
+export const Route = createFileRoute('/error')({
+  component: ErrorPage,
+
+  validateSearch: errorSearchSchema,
+
+  loaderDeps: ({ search }) => ({
+    status: search.status,
+    message: search.message,
+    name: search.name || 'No error name provided',
+  }),
+
+  loader: async ({ deps: { status, message, name } }) => {
+    return { status, message, name };
+  },
+});
+
+function ErrorPage() {
+  const error = useLoaderData({ from: '/error' });
+
+  return (
+    <section className="flex h-full w-full flex-col items-center justify-center">
+      <Error error={error as CustomError} />
+    </section>
+  );
+}
