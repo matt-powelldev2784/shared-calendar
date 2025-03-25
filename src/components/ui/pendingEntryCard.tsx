05cf6ff7c1dc8c364ad4ce4c48a53473d6c5b43c
Matthew Powell <matt.powell2784@gmail.com>
2025-03-25 21:15:03 +0000

    feat: allow user to reject pending request

--- src/components/ui/pendingEntryCard.tsx
+++ src/components/ui/pendingEntryCard.tsx
@@ -1,10 +1,11 @@
 import { Button } from '@/components/ui/button';
 import { CalendarCard } from '@/components/ui/calendarCard';
 import acceptPendingEntry from '@/db/acceptPendingEntry';
+import rejectPendingEntry from '@/db/rejectPendingEntry';
 import { getCalendarUrl } from '@/lib/getCalendarUrl';
 import type { CalendarEntry } from '@/ts/Calendar';
 import type { CustomError } from '@/ts/errorClass';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from '@tanstack/react-router';
 import { format } from 'date-fns';
 
@@ -15,8 +16,9 @@ type PendingEntryCardProps = {
 
 const PendingEntryCard = ({ entry, requestId }: PendingEntryCardProps) => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
-  const mutation = useMutation({
+  const acceptMutation = useMutation({
     mutationFn: async () => {
       await acceptPendingEntry({ entryId: entry.entryId, requestId });
     },
@@ -26,6 +28,7 @@ const PendingEntryCard = ({ entry, requestId }: PendingEntryCardProps) => {
         startDate: format(entry.startDate, 'yyyy-MM-dd'),
       });
 
+      queryClient.invalidateQueries({ queryKey: ['requests'] });
       navigate({ to: calendarUrl });
     },
     onError: (error: CustomError) => {
@@ -35,18 +38,43 @@ const PendingEntryCard = ({ entry, requestId }: PendingEntryCardProps) => {
     },
   });
 
+  const rejectMutation = useMutation({
+    mutationFn: async () => {
+      await rejectPendingEntry({ requestId });
+    },
+    onSuccess: () => {
+      const calendarUrl = getCalendarUrl({
+        calendarIds: entry.calendarId,
+      });
+
+      queryClient.invalidateQueries({ queryKey: ['requests'] });
+      navigate({ to: calendarUrl });
+    },
+    onError: (error: CustomError) => {
+      const status = error?.status || 500;
+      const message = 'Error rejecting pending entry';
+      navigate({ to: `/error?status=${status}&message=${message}` });
+    },
+  });
+
   return (
     <article className="border-primary/75 flex w-full flex-col gap-2 rounded-xl border-3 border-dashed p-2">
       <CalendarCard entry={entry} variant="white" />
       <div className="flex gap-2 p-2">
-        <Button variant="destructive" size="default" className="w-full">
+        <Button
+          variant="destructive"
+          size="default"
+          className="w-full"
+          onClick={() => rejectMutation.mutate()}
+        >
           Reject
         </Button>
+
         <Button
           size="default"
           className="w-full"
           onClick={() => {
-            mutation.mutate();
+            acceptMutation.mutate();
           }}
         >
           Accept
---
+++ src/db/rejectPendingEntry.ts
@@ -0,0 +1,48 @@
+import { doc, updateDoc, deleteDoc, getDoc } from 'firebase/firestore';
+import { db } from '@/db/firebaseConfig';
+import checkAuth from './checkAuth';
+import { CustomError } from '@/ts/errorClass';
+
+type RejectPendingEntry = { requestId: string };
+
+const rejectPendingEntry = async ({ requestId }: RejectPendingEntry) => {
+  try {
+    const user = await checkAuth();
+
+    if (!requestId) {
+      throw new CustomError(400, 'Request ID is required');
+    }
+
+    const requestDocRef = doc(db, 'requests', requestId);
+
+    // Fetch the request document
+    const requestDoc = await getDoc(requestDocRef);
+    if (!requestDoc.exists()) {
+      throw new CustomError(404, 'Request not found');
+    }
+
+    const requestData = requestDoc.data();
+    const requestUserIds = requestData?.requestedUserIds;
+
+    if (!requestUserIds) {
+      throw new CustomError(404, 'Invalid request data');
+    }
+
+    // remove the user from the request
+    // if no more users are in the request, delete the request document
+    if (requestUserIds.length === 1) {
+      await deleteDoc(requestDocRef);
+    } else {
+      await updateDoc(requestDocRef, {
+        requestedUserIds: requestUserIds.filter(
+          (id: string) => id !== user.uid,
+        ),
+      });
+    }
+  } catch (error) {
+    console.error('Error rejecting pending entry: ', error);
+    throw error;
+  }
+};
+
+export default rejectPendingEntry;
