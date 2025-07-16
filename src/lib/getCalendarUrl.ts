21789491cf47887a1ccfd89c2c447e383f8c6813
Matthew Powell <matt.powell2784@gmail.com>
2025-07-16 19:21:37 +0100

    feat: add start and end date to calendar url function

--- src/lib/getCalendarUrl.ts
+++ src/lib/getCalendarUrl.ts
@@ -4,14 +4,18 @@ type getCalendarUrlProps = {
   calendarIds: string;
   startDate?: string;
   daysToView?: number;
+  startHour?: number;
+  endHour?: number;
 };
 
 export const getCalendarUrl = ({
   calendarIds,
   startDate,
   daysToView,
+  startHour,
+  endHour,
 }: getCalendarUrlProps) => {
   const today = format(new Date(), 'yyyy-MM-dd');
-  const url = `/get-calendar?calendarIds=${calendarIds}&startDate=${startDate || today}&daysToView=${daysToView || 7}`;
+  const url = `/get-calendar?calendarIds=${calendarIds}&startDate=${startDate || today}&daysToView=${daysToView || 7}&startHour=${startHour || 8}&endHour=${endHour || 18}`;
   return url;
 };
