21c07a2efdafe89bf2cc6b5412a3acd985362f81
Matthew Powell <matt.powell2784@gmail.com>
2025-07-21 19:24:56 +0100

    fix: add get start of day when fetching calendar entries

--- src/lib/useResponsiveCalendarEntries.ts
+++ src/lib/useResponsiveCalendarEntries.ts
@@ -5,9 +5,6 @@ import { smallScreenSize } from './globalVariables';
 export function useResponsiveCalendarEntries(calendarEntries: CalendarEntriesData[]) {
   const [filteredEntries, setFilteredEntries] = useState(calendarEntries);
 
-  console.log('calendarEntries', calendarEntries);
-  console.log('filteredEntries', filteredEntries);
-
   useEffect(() => {
     const handleResize = () => {
       const isSmall = window.innerWidth < smallScreenSize;
